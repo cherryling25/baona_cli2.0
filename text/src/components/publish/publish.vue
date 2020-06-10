a1f8e74874507fbde94adb009de4ac4f355a708a
Cherry <cherryling25@outlook.com>
2020-06-10 23:25:51 +0800

    切换

--- text/src/components/publish/publish.vue
+++ text/src/components/publish/publish.vue
@@ -13,7 +13,111 @@
       </el-col>
     </el-row>
 
-    <div>
+
+    <!-- 选择任务类型 -->
+    <div v-if="selected == 0">
+      <el-row>
+        <el-col :span="22" :offset="1"><div class="grid bg-blue">第一步:选择任务类型和单数</div></el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="14" :offset="1">
+          <div class="grid space">
+            <el-checkbox v-model="checked">拼多多</el-checkbox>
+            <el-button type="primary" @click="dialogFormVisible = true">添加店铺</el-button>
+          </div>
+        </el-col>
+      </el-row>
+
+      <el-dialog title="添加店铺" :visible.sync="dialogFormVisible" :modal-append-to-body="false" width="30%">
+        <el-form :model="addShopForm">
+          <el-form-item label="平台" label-width="120px">
+            <el-select v-model="addShopForm.platform" placeholder="请选择">
+              <el-option label="拼多多" value="pingduoduo"></el-option>
+            </el-select>
+          </el-form-item>
+
+          <el-form-item label="店铺名称" label-width="120px">
+            <el-input v-model="addShopForm.name" autocomplete="off" placeholder="跟手机端宝贝页显示的店铺名一致"></el-input>
+          </el-form-item>
+
+          <el-form-item label="手机号" label-width="120px">
+            <el-input v-model="addShopForm.phone" autocomplete="off" placeholder="店铺手机号绑定后无法修改"></el-input>
+          </el-form-item>
+
+          <el-form-item label="拼多多商家后台登陆页面" label-width="120px">
+            <el-upload
+              class="avatar-uploader"
+              action="http://www.bn.com/index.php/index/Store/img"
+              :show-file-list="false"
+              :on-success="handleAvatarSuccess">
+              <img v-if="addShopForm.imageUrl" :src="addShopForm.imageUrl" class="avatar">
+              <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+            </el-upload>
+          </el-form-item>
+          
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="dialogFormVisible = false">取 消</el-button>
+          <el-button type="primary" @click="addShop">确 定</el-button>
+        </div>
+      </el-dialog>
+
+      <el-row>
+        <el-col :span="12" :offset="1">
+          <div class="grid space">
+            <el-radio v-model="radio" label="1">备选项1</el-radio>
+          </div>
+        </el-col>
+        <el-col :span="12" :offset="1">
+          <div class="grid space">
+            <el-radio v-model="radio" label="2">备选项2</el-radio>
+          </div>
+        </el-col>
+        <el-col :span="12" :offset="1">
+          <div class="grid space">
+            <el-radio v-model="radio" label="3">备选项3</el-radio>
+          </div>
+        </el-col>
+      </el-row>
+
+      <el-row>
+        <el-col :span="22" :offset="1"><div class="grid bg-blue">第二步:选择任务类型</div></el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12" :offset="1">
+          <div class="grid space">
+            <el-radio v-model="radio1" label="1">拼多多(普通垫付任务)</el-radio>
+          </div>
+        </el-col>
+        <el-col :span="12" :offset="1">
+          <div class="grid space">
+            <el-radio v-model="radio1" label="2">拼多多手机活动任务(限时秒杀等拼多多活动任务发布入口)</el-radio>
+          </div>
+        </el-col>
+      </el-row>
+
+      <el-row>
+        <el-col :span="22" :offset="1"><div class="grid bg-blue">第三步:平台服务</div></el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="14" :offset="1">
+          <div class="grid space">
+            <el-checkbox v-model="checked1">平台系统返款（收取0.5元/单的服务费）</el-checkbox>
+          </div>
+        </el-col>
+      </el-row>
+    
+    
+    
+    <el-row>
+      <el-col :span="12" :offset="10"><div class="grid">
+        <el-button style="margin-top: 12px;" @click="next" type="primary" plain>下一步</el-button>         
+      </div></el-col>
+    </el-row>
+    </div>
+
+    <!-- 填写任务类型 -->
+    <div v-if="selected == 1">
       <el-row>
         <el-col :span="22" :offset="1">
           <div class="grid bg-blue">第一步:填写商品信息</div>
@@ -128,115 +232,21 @@
       <el-row>
         <el-col :span="12" :offset="10">
           <div class="grid">
-              <el-button style="margin-top: 12px;" @click="next" type="primary" plain>下一步</el-button>
+            <el-button style="margin-top: 12px;" @click="pre" type="primary" plain>上一步</el-button>
+            <el-button style="margin-top: 12px;" @click="next" type="primary" plain>下一步</el-button>
           </div>
         </el-col>
       </el-row>
     </div>
 
-    <!-- 选择任务类型 -->
-    <!-- <div>
-      <el-row>
-        <el-col :span="22" :offset="1"><div class="grid bg-blue">第一步:选择任务类型和单数</div></el-col>
-      </el-row>
-      <el-row>
-        <el-col :span="14" :offset="1">
-          <div class="grid space">
-            <el-checkbox v-model="checked">拼多多</el-checkbox>
-            <el-button type="primary" @click="dialogFormVisible = true">添加店铺</el-button>
-          </div>
-        </el-col>
-      </el-row>
-
-      <el-dialog title="添加店铺" :visible.sync="dialogFormVisible" :modal-append-to-body="false" width="30%">
-        <el-form :model="addShopForm">
-          <el-form-item label="平台" label-width="120px">
-            <el-select v-model="addShopForm.platform" placeholder="请选择">
-              <el-option label="拼多多" value="pingduoduo"></el-option>
-            </el-select>
-          </el-form-item>
-
-          <el-form-item label="店铺名称" label-width="120px">
-            <el-input v-model="addShopForm.name" autocomplete="off" placeholder="跟手机端宝贝页显示的店铺名一致"></el-input>
-          </el-form-item>
-
-          <el-form-item label="手机号" label-width="120px">
-            <el-input v-model="addShopForm.phone" autocomplete="off" placeholder="店铺手机号绑定后无法修改"></el-input>
-          </el-form-item>
-
-          <el-form-item label="拼多多商家后台登陆页面" label-width="120px">
-            <el-upload
-              class="avatar-uploader"
-              action="http://www.bn.com/index.php/index/Store/img"
-              :show-file-list="false"
-              :on-success="handleAvatarSuccess">
-              <img v-if="addShopForm.imageUrl" :src="addShopForm.imageUrl" class="avatar">
-              <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-            </el-upload>
-          </el-form-item>
-          
-        </el-form>
-        <div slot="footer" class="dialog-footer">
-          <el-button @click="dialogFormVisible = false">取 消</el-button>
-          <el-button type="primary" @click="addShop">确 定</el-button>
-        </div>
-      </el-dialog>
-
-      <el-row>
-        <el-col :span="12" :offset="1">
-          <div class="grid space">
-            <el-radio v-model="radio" label="1">备选项1</el-radio>
-          </div>
-        </el-col>
-        <el-col :span="12" :offset="1">
-          <div class="grid space">
-            <el-radio v-model="radio" label="2">备选项2</el-radio>
-          </div>
-        </el-col>
-        <el-col :span="12" :offset="1">
-          <div class="grid space">
-            <el-radio v-model="radio" label="3">备选项3</el-radio>
-          </div>
-        </el-col>
-      </el-row>
-
-      <el-row>
-        <el-col :span="22" :offset="1"><div class="grid bg-blue">第二步:选择任务类型</div></el-col>
-      </el-row>
-      <el-row>
-        <el-col :span="12" :offset="1">
-          <div class="grid space">
-            <el-radio v-model="radio1" label="1">拼多多(普通垫付任务)</el-radio>
-          </div>
-        </el-col>
-        <el-col :span="12" :offset="1">
-          <div class="grid space">
-            <el-radio v-model="radio1" label="2">拼多多手机活动任务(限时秒杀等拼多多活动任务发布入口)</el-radio>
-          </div>
-        </el-col>
-      </el-row>
-
-      <el-row>
-        <el-col :span="22" :offset="1"><div class="grid bg-blue">第三步:平台服务</div></el-col>
-      </el-row>
-      <el-row>
-        <el-col :span="14" :offset="1">
-          <div class="grid space">
-            <el-checkbox v-model="checked1">平台系统返款（收取0.5元/单的服务费）</el-checkbox>
-          </div>
-        </el-col>
-      </el-row>
-    
-    
-    
-    <el-row>
-      <el-col :span="12" :offset="10"><div class="grid">
-         <router-link to="/taskInfo">
-            <el-button style="margin-top: 12px;" @click="next" type="primary" plain>下一步</el-button>         
-          </router-link>
-      </div></el-col>
-    </el-row>
-    </div>-->
+    <div v-if="selected == 2">
+      发布
+      <el-button style="margin-top: 12px;" @click="pre" type="primary" plain>上一步</el-button>
+      <el-button style="margin-top: 12px;" @click="next" type="primary" plain>下一步</el-button>
+    </div>
+    <div v-if="selected == 3">
+      成功
+    </div>
   </div>
 </template>
 
@@ -255,23 +265,37 @@ export default {
       radio: "1",
       radio1: "1",
 
+      input2: '',
       merchantInput: '',
       commodityInput: '',
       imageUrl: '',
 
-      // dialogFormVisible: false,
-      // addShopForm: {
-      //   platform: "",
-      //   name: "",
-      //   phone: "",
-      //   imageUrl: ""
-      // }
+      selected: 0,
+
+      dialogFormVisible: false,
+      addShopForm: {
+        platform: "",
+        name: "",
+        phone: "",
+        imageUrl: ""
+      }
     };
   },
 
   methods: {
     next() {
-      if (this.active++ > 2) this.active = 0;
+      this.selected++;
+      if (this.active++ > 2) { 
+        this.active = 0; 
+        this.selected = 0;
+      }
+    },
+    pre(){
+      this.selected--;
+      if (this.active-- == 0) { 
+        this.active = 0; 
+        this.selected = 0;
+      }
     },
     //上传图片
     handleAvatarSuccess(res, file) {
